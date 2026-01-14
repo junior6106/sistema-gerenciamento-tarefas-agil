1b903a306e803c8da60dd39e2dcf8dd2a6442b48
junior6106 <joao190085@gmail.com>
2026-01-14 19:41:00 -0300

    test: adiciona teste automatizado para criação de tarefas

---
+++ tests/test_tasks.py
@@ -0,0 +1,7 @@
+from src.app import create_task
+
+def test_create_task():
+    task = create_task("Estudar Engenharia de Software", "Alta")
+    assert task["title"] == "Estudar Engenharia de Software"
+    assert task["priority"] == "Alta"
+    assert task["status"] == "To Do"
